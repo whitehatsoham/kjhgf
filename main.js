f563b08f2e47f370b00df311664b81845d099b75
whitehatsoham <100528214+whitehatsoham@users.noreply.github.com>
2022-10-23 18:54:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function modelLoaded() {
+  console.log('Model Leaded!');
+}
+function draw(){
+image(video, 0, 0, 300, 300); 
+classifier.classify(video, gotResult);
+}
+var previouse_result = '';
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else{
+  if((results[0].confidecse > 0.5) && (previous_result != results[0].label)){
+    console.log(results);
+    previous_result = results[0].label;
+    var synth = window.speechSynthesis;
+    speak_data = 'Object detected is - '+results[0].label;
+    var utterThis = new SpeechSynthesisisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_ovject_accuracy").innerHTML = results
+    [0].confidence.toFixed(3);
+
+  }
+}
+}
